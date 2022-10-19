d453faea841c0bf85abc0b5bacb2f4b630006b6b
vikasgautam1234 <vikasmtr1234@gmail.com>
2022-10-19 23:18:15 +0530

    both questions done

--- src/routes/route.js
+++ src/routes/route.js
@@ -108,5 +108,44 @@ router.get("/films/:filmId", function(req, res){
        res.send("The film id doesn't match any movie")
 })
 
+
+// question 1 -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+router.get("/sol1", function (req, res) {
+
+let arr= [1,2,3,5,6,7]
+
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+
+   let lastDigit= arr.pop()
+   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+   let missingNumber= consecutiveSum - total
+
+   res.send(  { data: missingNumber  }  );
+ });
+
+
+// question 2 -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+
+router.get("/sol2", function (req, res) {
+let arr= [33, 34, 35, 37, 38]
+   let len= arr.length
+
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+
+   let firstDigit= arr[0]
+   let lastDigit= arr.pop()
+   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+   let missingNumber= consecutiveSum - total
+  
+   res.send(  { data: missingNumber  }  );
+ });
+
+
 module.exports = router;
 // adding this comment for no reason
