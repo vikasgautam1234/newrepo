adc44fe6cbd6544388cf82e858a58f1881e1460b
vikasgautam1234 <vikasmtr1234@gmail.com>
2022-10-20 23:20:28 +0530

    voting age done

--- src/routes/route.js
+++ src/routes/route.js
@@ -147,5 +147,60 @@ let arr= [33, 34, 35, 37, 38]
  });
 
 
+
+
+
+//  you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ router.post("/validage",function(req,res){
+     const voting = req.query.age
+     const newarr = []
+     for (let index = 0; index < persons.length; index++) {
+        let element = persons[index];
+        if((element.age>=voting)){
+            
+            element.votingStatus= true
+            newarr.push(element)
+            
+        }   
+        }res.send({final: newarr    })
+        // console.log(persons)
+        
+    
+ })
+ 
+
+
+
 module.exports = router;
 // adding this comment for no reason
