0702744e41b9a373728ce126ab5972a8b16ef98e
vikasgautam1234 <vikasmtr1234@gmail.com>
2022-11-01 21:19:49 +0530

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 const express = require('express');
+const moment = require("moment");
+const IP = require("ip");
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+// const moment = require('moment/moment.js');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://vikasgautam:8279787711@cluster0.anirnnm.mongodb.net/mongodbmiddleware", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+
+    const globalApi = function (req, res, next) {
+        var CurrentDate = moment().format('YYYY-MM-DD'+" "+ 'h:m:s'+" , "+IP.address());
+        console.log("heated api path:", req.originalUrl);
+        console.log(CurrentDate);
         next();
-  }
-  );
+    }
+
+  app.use(globalApi);  //it will log everytime whenever would be any API(post,get,put,etc) hits
 
 app.use('/', route);
 
